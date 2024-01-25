d72617f0c87f8a9944d21c1602c54ce7c8974c51
Gyemfa-Gloria-Abedi <akosuagyemfa98@gmail.com>
2024-01-25 09:36:42 +0000

    setup admin dashboard

--- velio-restaurant/src/App.tsx
+++ velio-restaurant/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import ClientDashboard from "./views/ClientDashboard"
 import React from "react";
 import Dashboard from "./views/Dashboard";
+import AdminDashboard from "./views/AdminDashboard";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={ <Dashboard/>} />
         <Route path="/velio-restuarant-client" element={<ClientDashboard/> } />
+        <Route path="/velio-restuarant-admin" element={<AdminDashboard/>}/>
       </Routes>
     </>
   )
