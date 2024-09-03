1dfb00114322ea6aca63f961e9692c23cb198eef
Gyemfa-Gloria-Abedi <akosuagyemfa98@gmail.com>
2024-09-03 16:38:58 +0000

    feature: api setup

---
+++ velio-restaurant-api/dist/index.js
@@ -0,0 +1,27 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// server.js
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+// Middleware
+app.use(cors());
+app.use(express.json());
+// Routes
+// app.post('/submit-form', async (req, res) => {
+//   try {
+//     const formData = new FormModel(req.body);
+//     await formData.save();
+//     res.status(201).json({ message: 'Form data saved successfully' });
+//   } catch (error) {
+//     res.status(500).json({ error: 'Error saving form data' });
+//   }
+// });
+app.get("/", async (req, res) => {
+    res.status(200).json({ message: "Hello world" });
+});
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
